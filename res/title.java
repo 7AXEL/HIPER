e3168007adbb0e4e51c1dd2807cd42ae20895e6f
A.X.E.L <108288065+7AXEL@users.noreply.github.com>
2022-12-29 20:56:17 +0100

    Add files via upload

---
+++ res/title.java
@@ -0,0 +1,11 @@
+public class title {
+  public static void main (String [] args) {
+    System.out.println("\033[1;92m┏━━━┓┏━┓┏━┓┏━━━┓┏┓");
+    System.out.println("\033[1;92m┃┏━┓┃┗┓┗┛┏┛┃┏━━┛┃┃ \033[0;97m\033[1;104mA.X.E.L");
+    System.out.println("\033[0;0m\033[1;92m┃┃╋┃┃╋┗┓┏┛╋┃┗━━┓┃┃ \033[0;97m\033[1;101mHIPER");
+    System.out.println("\033[0;0m\033[1;92m┃┗━┛┃╋┏┛┗┓╋┃┏━━┛┃┃╋┏┓");
+    System.out.println("\033[1;92m┃┏━┓┣┳┛┏┓┗┳┫┗━━┳┫┗━┛┃");
+    System.out.println("\033[1;92m┗┛╋┗┻┻━┛┗━┻┻━━━┻┻━━━┛\t\033[1;93mVERSION:1.0");
+    System.out.println("------------------------------------");
+  }
+}
